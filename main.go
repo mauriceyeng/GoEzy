e0b691e10a3d71f1da767bfd8ce56ca08bf90bdd
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-08 19:59:31 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"project/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", handlers.Home)
+	router.Run("localhost:8080")
+}
